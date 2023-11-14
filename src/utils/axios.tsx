b33475c9c43af2313e982a159a1d4224037de988
Alex Thai <tthai21@student.holmesglen.edu.au>
2023-11-15 05:51:44 +1100

    update url

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://employeesappa1.azurewebsites.net",
+  baseURL: "https://employeesappa1.azurewebsites.net/",
 });
